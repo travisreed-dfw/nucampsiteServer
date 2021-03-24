2b2f1f6e5281057b8141119e7de66c3062e013ec
Travis Reed <traviswreed2@gmail.com>
2021-03-24 17:47:36 -0500

    Fixed if .equals()

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -15,29 +15,37 @@ partnerRouter
 			})
 			.catch((err) => next(err));
 	})
-	.post(authenticate.verifyAdmin, (req, res, next) => {
-		Partner.create(req.body)
-			.then((partner) => {
-				console.log("Partner Created ", partner);
-				res.statusCode = 200;
-				res.setHeader("Content-Type", "application/json");
-				res.json(partner);
-			})
-			.catch((err) => next(err));
-	})
+	.post(
+		authenticate.verifyUser,
+		authenticate.verifyAdmin,
+		(req, res, next) => {
+			Partner.create(req.body)
+				.then((partner) => {
+					console.log("Partner Created ", partner);
+					res.statusCode = 200;
+					res.setHeader("Content-Type", "application/json");
+					res.json(partner);
+				})
+				.catch((err) => next(err));
+		}
+	)
 	.put(authenticate.verifyUser, (req, res) => {
 		res.statusCode = 403;
 		res.end("PUT operation not supported on /partners");
 	})
-	.delete(authenticate.verifyAdmin, (req, res, next) => {
-		Partner.deleteMany()
-			.then((response) => {
-				res.statusCode = 200;
-				res.setHeader("Content-Type", "application/json");
-				res.json(response);
-			})
-			.catch((err) => next(err));
-	});
+	.delete(
+		authenticate.verifyUser,
+		authenticate.verifyAdmin,
+		(req, res, next) => {
+			Partner.deleteMany()
+				.then((response) => {
+					res.statusCode = 200;
+					res.setHeader("Content-Type", "application/json");
+					res.json(response);
+				})
+				.catch((err) => next(err));
+		}
+	);
 
 partnerRouter
 	.route("/:partnerId")
@@ -56,30 +64,38 @@ partnerRouter
 			`POST operation not supported on /partners/${req.params.partnerId}`
 		);
 	})
-	.put(authenticate.verifyAdmin, (req, res, next) => {
-		Partner.findByIdAndUpdate(
-			req.params.partnerId,
-			{
-				$set: req.body,
-			},
-			{ new: true }
-		)
-			.then((partner) => {
-				res.statusCode = 200;
-				res.setHeader("Content-Type", "application/json");
-				res.json(partner);
-			})
-			.catch((err) => next(err));
-	})
-	.delete(authenticate.verifyAdmin, (req, res, next) => {
-		Partner.findByIdAndDelete(req.params.partnerId)
-			.then((response) => {
-				res.statusCode = 200;
-				res.setHeader("Content-Type", "application/json");
-				res.json(response);
-			})
-			.catch((err) => next(err));
-	});
+	.put(
+		authenticate.verifyUser,
+		authenticate.verifyAdmin,
+		(req, res, next) => {
+			Partner.findByIdAndUpdate(
+				req.params.partnerId,
+				{
+					$set: req.body,
+				},
+				{ new: true }
+			)
+				.then((partner) => {
+					res.statusCode = 200;
+					res.setHeader("Content-Type", "application/json");
+					res.json(partner);
+				})
+				.catch((err) => next(err));
+		}
+	)
+	.delete(
+		authenticate.verifyUser,
+		authenticate.verifyAdmin,
+		(req, res, next) => {
+			Partner.findByIdAndDelete(req.params.partnerId)
+				.then((response) => {
+					res.statusCode = 200;
+					res.setHeader("Content-Type", "application/json");
+					res.json(response);
+				})
+				.catch((err) => next(err));
+		}
+	);
 
 partnerRouter
 	.route("/:partnerId/comments")
--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -15,16 +15,20 @@ promotionRouter
 			})
 			.catch((err) => next(err));
 	})
-	.post(authenticate.verifyAdmin, (req, res, next) => {
-		Promotion.create(req.body)
-			.then((promotion) => {
-				console.log("Promotion Created ", promotion);
-				res.statusCode = 200;
-				res.setHeader("Content-Type", "application/json");
-				res.json(promotion);
-			})
-			.catch((err) => next(err));
-	})
+	.post(
+		authenticate.verifyUser,
+		authenticate.verifyAdmin,
+		(req, res, next) => {
+			Promotion.create(req.body)
+				.then((promotion) => {
+					console.log("Promotion Created ", promotion);
+					res.statusCode = 200;
+					res.setHeader("Content-Type", "application/json");
+					res.json(promotion);
+				})
+				.catch((err) => next(err));
+		}
+	)
 	.put(authenticate.verifyUser, (req, res) => {
 		res.statusCode = 403;
 		res.end("PUT operation not supported on /promotions");
@@ -56,30 +60,38 @@ promotionRouter
 			`POST operation not supported on /promotions/${req.params.promotionId}`
 		);
 	})
-	.put(authenticate.verifyAdmin, (req, res, next) => {
-		Promotion.findByIdAndUpdate(
-			req.params.promotionId,
-			{
-				$set: req.body,
-			},
-			{ new: true }
-		)
-			.then((promotion) => {
-				res.statusCode = 200;
-				res.setHeader("Content-Type", "application/json");
-				res.json(promotion);
-			})
-			.catch((err) => next(err));
-	})
-	.delete(authenticate.verifyAdmin, (req, res, next) => {
-		Promotion.findByIdAndDelete(req.params.promotionId)
-			.then((response) => {
-				res.statusCode = 200;
-				res.setHeader("Content-Type", "application/json");
-				res.json(response);
-			})
-			.catch((err) => next(err));
-	});
+	.put(
+		authenticate.verifyUser,
+		authenticate.verifyAdmin,
+		(req, res, next) => {
+			Promotion.findByIdAndUpdate(
+				req.params.promotionId,
+				{
+					$set: req.body,
+				},
+				{ new: true }
+			)
+				.then((promotion) => {
+					res.statusCode = 200;
+					res.setHeader("Content-Type", "application/json");
+					res.json(promotion);
+				})
+				.catch((err) => next(err));
+		}
+	)
+	.delete(
+		authenticate.verifyUser,
+		authenticate.verifyAdmin,
+		(req, res, next) => {
+			Promotion.findByIdAndDelete(req.params.promotionId)
+				.then((response) => {
+					res.statusCode = 200;
+					res.setHeader("Content-Type", "application/json");
+					res.json(response);
+				})
+				.catch((err) => next(err));
+		}
+	);
 
 promotionRouter
 	.route("/:promotionId/comments")
