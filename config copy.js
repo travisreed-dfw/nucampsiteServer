4c90db962516b98a7a8b509d98b8dd20c2a81f1d
Travis Reed <traviswreed2@gmail.com>
2021-03-26 15:02:25 -0500

    Added copies of public\index.html and config.js that don't have private keys in them

---
+++ config copy.js
@@ -0,0 +1,8 @@
+module.exports = {
+	secretKey: "12345-67890-09876-54321",
+	mongoUrl: "mongodb://localhost:27017/nucampsite",
+	facebook: {
+		clientId: " FACEBOOK APP ID GOES HERE ",
+		clientSecret: " FACEBOOK CLIENT SECRET GOES HERE ",
+	},
+};
