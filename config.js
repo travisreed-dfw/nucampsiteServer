f39885364549a9b21f7fdc3d9455e5f64ac60871
Travis Reed <traviswreed2@gmail.com>
2021-03-26 14:46:01 -0500

    test

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
 	secretKey: "12345-67890-09876-54321",
 	mongoUrl: "mongodb://localhost:27017/nucampsite",
 	facebook: {
-		clientId: "APP ID",
-		clientSecret: " CLIENT SECRET",
+		clientId: "147098060658836",
+		clientSecret: "25cb7dceae4896193aa47e245519b0a8",
 	},
 };
